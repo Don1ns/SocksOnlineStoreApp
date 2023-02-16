6c36979d45a2c096c3d0c32f9b66ee363a003deb
Don1ns <riaz.karimullin@yandex.ru>
2023-02-16 13:35:09 +0300

    Курсовая работа 3

---
+++ src/test/java/coursework/socksonlinestoreapp/SocksOnlineStoreAppApplicationTests.java
@@ -0,0 +1,13 @@
+package coursework.socksonlinestoreapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocksOnlineStoreAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
